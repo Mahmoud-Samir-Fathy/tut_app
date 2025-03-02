b08bb5ca05e24d6d4c91e7d9fca80457bea10d64
Mahmoud-Samir-Fathy <mahmoudsamir0777@gmail.com>
2025-03-02 02:05:33 +0200

    fonts and colors organized

---
+++ lib/presentaion/color_manager.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class ColorManager{
+  static Color primaryColor=const Color(0xffED9728);
+  static Color titleColor=const Color(0xff525252);
+  static Color whiteColor=const Color(0xffFFFFFF);
+  static Color textColor=const Color(0xff9E9E9E);
+  static Color borderColor=const Color(0xff304654);
+  static Color navIconsColor=const Color(0xff858585);
+}
