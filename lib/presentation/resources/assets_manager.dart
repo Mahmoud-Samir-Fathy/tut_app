cfda859ed8669d01bab61acbcd9777ea55394a92
Mahmoud-Samir-Fathy <mahmoudsamir0777@gmail.com>
2025-03-05 19:17:42 +0200

    splash screen added

---
+++ lib/presentation/resources/assets_manager.dart
@@ -0,0 +1,5 @@
+const String imagePath = 'assets/images';
+
+class AssetsManager {
+  static String splashLogo = '$imagePath/splash_logo.png';
+}
--- lib/presentation/splash_screen/splash_screen.dart
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/resources/assets_manager.dart';
+import 'package:tut_app/presentation/resources/color_manager.dart';
+import 'package:tut_app/presentation/resources/constant_manager.dart';
+import 'package:tut_app/presentation/resources/routes_manager.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,8 +14,35 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  _startDelay() {
+    _timer = Timer(const Duration(seconds: ConstantManager.splashDelay), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingScreen);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorManager.primaryColor,
+      body: Center(
+        child: Image(image: AssetImage(AssetsManager.splashLogo)),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 }
